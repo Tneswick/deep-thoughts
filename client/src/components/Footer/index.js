a4d635e9cfa9ffc644e342a7659b330ff4f7619f
Deck Stream <tneswick2@verizon.net>
2022-06-18 12:50:04 -0700

    feature/routes completed

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy;2022 by Tneswick</div>
+      <div className="container">
+        &copy;{new Date().getFullYear()} by <a href="https://www.github.com/Tneswick">Tneswick</a>
+      </div>
     </footer>
   );
 };
